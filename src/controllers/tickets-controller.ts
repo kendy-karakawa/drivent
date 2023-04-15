42f6096b874106c800d0de7e1520aab8e5717f27
kendy-karakawa <kendy.karakawa@gmail.com>
2023-04-15 00:38:50 -0300

    fix: ticket controller error to next(error)

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,32 +1,31 @@
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 import httpStatus from 'http-status';
 import { TicketType } from '@prisma/client';
 import { AuthenticatedRequest } from '@/middlewares';
 import ticketsService from '@/services/tickets-service';
 import { UserTicket } from '@/protocols';
 
-export async function getAllTickets(req: AuthenticatedRequest, res: Response) {
+export async function getAllTickets(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   try {
     const ticket: TicketType[] = await ticketsService.getAllTickets();
 
     res.status(httpStatus.OK).send(ticket);
   } catch (error) {
-    return res.sendStatus(500);
+    next(error);
   }
 }
 
-export async function getUserTickets(req: AuthenticatedRequest, res: Response) {
+export async function getUserTickets(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const id: number = req.userId;
   try {
     const ticket: UserTicket = await ticketsService.getUserTickets(id);
     res.status(httpStatus.OK).send(ticket);
   } catch (error) {
-    if (error.name === 'NotFoundError') return res.status(httpStatus.NOT_FOUND).send(error.message);
-    return res.sendStatus(500);
+    next(error);
   }
 }
 
-export async function postTicket(req: AuthenticatedRequest, res: Response) {
+export async function postTicket(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const ticketTypeId: number = req.body.ticketTypeId;
   const id: number = req.userId;
 
@@ -34,7 +33,6 @@ export async function postTicket(req: AuthenticatedRequest, res: Response) {
     const ticket: UserTicket = await ticketsService.postTicket(ticketTypeId, id);
     res.status(httpStatus.CREATED).send(ticket);
   } catch (error) {
-    if (error.name === 'NotFoundError') return res.status(httpStatus.NOT_FOUND).send(error.message);
-    return res.sendStatus(500);
+    next(error);
   }
 }
