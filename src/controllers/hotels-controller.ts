f70bd6e66ea6409da15b7fcd377bdb4439a8ac41
kendy-karakawa <kendy.karakawa@gmail.com>
2023-04-21 10:53:02 -0300

    feat get hotel error in service

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -4,8 +4,9 @@ import { AuthenticatedRequest } from '@/middlewares';
 import hotelsService from '@/services/hotels-service';
 
 export async function getAllHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const userId: number = req.userId;
   try {
-    const hotels = await hotelsService.getAllHotels();
+    const hotels = await hotelsService.getAllHotels(userId);
     res.status(httpStatus.OK).send(hotels);
   } catch (error) {
     next(error);
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,6 +1,16 @@
+import { TicketStatus } from '@prisma/client';
+import { notFoundError, paymentRequiredError } from '@/errors';
+import { UserTicket } from '@/protocols';
 import hotelsRepository from '@/repositories/hotels-repository';
+import ticketsRepository from '@/repositories/tickets-repository';
 
-async function getAllHotels() {
+async function getAllHotels(userId: number) {
+  const ticket: UserTicket = await ticketsRepository.getUserTickets(userId);
+  if (!ticket) throw notFoundError();
+  if (ticket.TicketType.includesHotel === false) throw paymentRequiredError();
+  if (ticket.TicketType.isRemote === true) throw paymentRequiredError();
+  if (ticket.status === TicketStatus.RESERVED) throw paymentRequiredError();
+
   const hotels = await hotelsRepository.getAllHotels();
 
   return hotels;
