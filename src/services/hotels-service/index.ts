d44cdad276c25ab4c4639b9d2a86b74dacbfc993
kendy-karakawa <kendy.karakawa@gmail.com>
2023-04-24 21:08:27 -0300

    feat listHotels in service

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -5,15 +5,19 @@ import hotelsRepository from '@/repositories/hotels-repository';
 import ticketsRepository from '@/repositories/tickets-repository';
 import enrollmentRepository from '@/repositories/enrollment-repository';
 
-async function getAllHotels(userId: number) {
+async function listHotels(userId: number) {
   const enrollment: Enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
   if (!enrollment) throw notFoundError();
 
   const ticket: UserTicket = await ticketsRepository.getUserTickets(enrollment.id);
   if (!ticket) throw notFoundError();
-  if (ticket.TicketType.includesHotel === false) throw paymentRequiredError();
-  if (ticket.TicketType.isRemote === true) throw paymentRequiredError();
-  if (ticket.status === TicketStatus.RESERVED) throw paymentRequiredError();
+  if (!ticket.TicketType.includesHotel || ticket.TicketType.isRemote || ticket.status === TicketStatus.RESERVED) {
+    throw paymentRequiredError();
+  }
+}
+
+async function getAllHotels(userId: number) {
+  await listHotels(userId);
 
   const hotels = await hotelsRepository.getAllHotels();
   if (hotels.length === 0) throw notFoundError();
@@ -21,21 +25,12 @@ async function getAllHotels(userId: number) {
 }
 
 async function getHotelRooms(userId: number, hotelId: number) {
-  const enrollment: Enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
-  if (!enrollment) throw notFoundError();
+  await listHotels(userId);
 
   const hotel = await hotelsRepository.getHotelById(hotelId);
   if (!hotel) throw notFoundError();
 
-  const ticket: UserTicket = await ticketsRepository.getUserTickets(enrollment.id);
-  if (!ticket) throw notFoundError();
-
-  if (ticket.TicketType.includesHotel === false) throw paymentRequiredError();
-  if (ticket.TicketType.isRemote === true) throw paymentRequiredError();
-  if (ticket.status === TicketStatus.RESERVED) throw paymentRequiredError();
-
   const hotelRooms = await hotelsRepository.getHotelRooms(hotelId);
-
   return hotelRooms;
 }
 
